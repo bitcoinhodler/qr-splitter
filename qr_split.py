00a10fbaece2b881ad0addfb61cda431c8df70f0
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2020-12-06 23:13:47 +0000

    Actually read input

--- qr_split.py
+++ qr_split.py
@@ -2,19 +2,24 @@
 """Read stdin, output HTML with QR codes."""
 
 import argparse
+import sys
 
 
 def get_cmdline_args():
     """Parse cmdline and return Namespace."""
     parser = argparse.ArgumentParser(
-        description="Multi QR code generator: reads data from stdin, "
-        "writes HTML for printing to stdout.",
+        description="Multi QR code generator: reads data from file "
+        "(or stdin), writes HTML for printing to stdout.",
     )
     parser.add_argument('--title', help="title for page footer")
     parser.add_argument(
         '--version', type=int, default=16,
         help="QR code version to use (1-40, default %(default)s)",
     )
+    parser.add_argument('input', metavar='FILE', nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help="input file (stdin if none)")
     return parser.parse_args()
 
 
@@ -22,6 +27,8 @@ def main():
     """Run main cmdline program."""
     args = get_cmdline_args()
     print(str(args))
+    indata = args.input.read()
+    print("Input is:", indata)
 
 
 if __name__ == "__main__":
